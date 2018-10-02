cc2ddaa669fc5ac216154e691f892af64c63d3cd
ruslan.yaganov <kesha@kesha>
2018-10-02 14:12:07 +0300

    testing with makefiles

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <stdio.h>
-
+#include <math.h>
+#include "functions.h"
 int main()
 {
-	printf("Hello World\n");
-	return 0;
+print_hello();
+printf("\n");
+double a, b, c;
+scanf("%lf", &a);
+scanf("%lf", &b);
+scanf("%lf", &c);
+double D = (b*b) - 4*a*c;
+if(D >= 0)
+{
+ double x1 = (-b+sqrt(D))/(2*a);
+ double x2 = (-b-sqrt(D))/(2*a);
+ if(x1 == x2) {printf("%lf \n", x1);}
+ else {printf("%lf \n", x1); printf("%lf \n", x2);}
+}
+else
+{
+	printf("No roots\n");
+}
+return 0;
 }
